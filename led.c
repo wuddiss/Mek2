70a1e979f8f35bb6e6f2238a69f73115a3f47bad
wuddiss <118975959+wuddiss@users.noreply.github.com>
2024-02-08 17:42:28 +0000

    Create led.c

---
+++ led.c
@@ -0,0 +1,21 @@
+/*
+ * led.c
+ *
+ *  Created on: Feb 4, 2024
+ *      Author: vidar
+ */
+#include <stdio.h>
+#include "led.h"
+#include <avr/io.h>
+
+void initLED() {
+	DDRB |= 0b00100000; // Set LED sem útgang 13
+}
+
+void LED_on() {
+	PORTB &= ~0b00100000; // LED útgangur 13 á
+}
+
+void LED_off() {
+	PORTB |= 0b00100000; // LED útgangur 13 aff
+}
