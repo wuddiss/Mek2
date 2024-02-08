4734d8c20fa7ebc29bc2f6098139867875d0d394
wuddiss <118975959+wuddiss@users.noreply.github.com>
2024-02-08 17:43:32 +0000

    Create button.h

---
+++ button.h
@@ -0,0 +1,14 @@
+/*
+ * button.h
+ *
+ *  Created on: Feb 4, 2024
+ *      Author: vidar
+ */
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+void initButton();
+int isButtonPressed();
+
+#endif /* BUTTON_H_ */
