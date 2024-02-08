314a3c826efc9780f78ec527919bdaea3382ffb1
wuddiss <118975959+wuddiss@users.noreply.github.com>
2024-02-08 18:57:42 +0000

    Create stepper.h

---
+++ stepper.h
@@ -0,0 +1,13 @@
+/*
+ * stepper.h
+ *
+ *  Created on: Feb 8, 2024
+ *      Author: vidar
+ */
+#ifndef STEPPER_H_
+#define STEPPER_H_
+
+void stepper_rs(int speed);
+void stepper_ws(int speed);
+
+#endif
