49e84356e016a3dd3d707e197aaef439879855d6
wuddiss <118975959+wuddiss@users.noreply.github.com>
2024-02-15 08:02:18 +0000

    Update stepper.h

--- stepper.h
+++ stepper.h
@@ -14,6 +14,8 @@ void steps_wd_bw(int speed, int steps);
 void step_wd_fw(int speed);
 void step_wd_bw(int speed);
 
+int steps_fsd_fw(int speed, int steps, int next_step);
+void steps_fsd_bw(int speed, int steps);
 void step_fsd_fw(int speed);
 void step_fsd_bw(int speed);
 
@@ -25,3 +27,4 @@ void full_step_drive(int step);
 void half_step_drive(int step);
 
 #endif
+
