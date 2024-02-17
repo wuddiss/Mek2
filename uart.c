7837e462674ee67cc7c1459dc67ac9f887b2bd24
wuddiss <118975959+wuddiss@users.noreply.github.com>
2024-02-17 20:46:38 +0000

    Update uart.c

--- uart.c
+++ uart.c
@@ -1,26 +1,138 @@
- #include <avr/io.h>
+/*
+ * Uart.c
+ *
+ *  Created on: Feb 17, 2024
+ *      Author: vidar
+ */
+#include "uart.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
- int main()
- {
- char cData ;
- // b a u d r a te 4800
- UBRR0H = 0;
- UBRR0L = 207;
- // e n a bl e r e c e i v e r and t r a n sm i t t e r
- UCSR0B = (1<<RXEN0)|(1<<TXEN0);
- // s e t frame forma t
- UCSR0C=(1<<USBS0)|(3<< UCSZ00);
+#define BUFFERSIZE 32
 
- while (1)
+int TxBuffer[BUFFERSIZE]; //ring buffer
+int iIn; //iIn points to next free space in buffer
+int iOut; //iOut point to oldest data in buffe r
+int cData;
+
+//buffer is empty if iIn==iOut
+//buffer is full if next(iIn)==iOut
+
+// increment index and wrap around if at end
+int NextIndex(int index)
+{
+ index++;
+ if(index >= BUFFERSIZE)
+ index=0;
+ return index;
+}
+
+//return 1 if buffer is full, else return 0
+char TxBufferEmpty()
+{
+ if(iIn==iOut) return 1;
+ else return 0;
+}
+
+//return 1 if buffer full, else return 0
+char TxBufferFull()
+{
+ if(NextIndex(iIn)==iOut) return 1;
+ else return 0;
+}
+
+//return 1 if success, 0 if error
+char TxBufferAddChar(char cData)
+{
+ char retval = 1; //default: success
+ if(!TxBufferFull())
  {
- // w a i t f o r da ta t o be r e c e i v e d
- while (!(UCSR0A&(1<<RXC0)));
- // g e t and r e t u r n da ta
- cData= UDR0;
- // w a i t f o r empty b u f f e r
- while (!(UCSR0A&(1<<UDRE0)));
- // pu t da ta i n t o b u f f e r
- UDR0=cData+0x01;
+   TxBuffer[iIn] = cData;
+   iIn = NextIndex(iIn); //advance in-pointer
  }
- return 0;
+ else retval=0; //don't touch buffer, indicate error
+ return retval;
  }
+
+char TxBufferReadChar(char* pData)
+{
+ char retval = 1; //default: success
+ if(!TxBufferEmpty())
+ {
+   *pData = TxBuffer[iOut];
+   iOut = NextIndex(iOut);
+ }
+ else retval = 0;
+ return retval;
+}
+
+//return 1 if succes, O if error
+char TxPrintString(char* pChar)
+{
+ char cSuccess = 1; //default: succes s
+ char cData;
+ while(*pChar != 0)
+ {
+   if(TxBufferAddChar(*pChar))
+   ; //success, do nothing
+   else{
+	   cSuccess = 0;//indicate error
+       break ; //brake the whileloop
+       }
+   pChar++;//point to next char
+ }
+ //manually start the writing by writing the first char
+ if(UCSR0A&(1<<UDRE0))
+ {
+   if(TxBufferReadChar(&cData))
+     UDR0=cData;
+   else
+     cSuccess=0;
+ }
+ return cSuccess;
+}
+
+void Init_Uart()
+{
+ //baudrate = 57600
+ UBRR0H=0;
+ UBRR0L=16;
+
+ //enable receiver and transmitter, Rx complete interrupt enable
+ UCSR0B = (1<<RXEN0)|(1<<TXEN0)|(1<<TXCIE0);
+ //set frame format
+ UCSR0C=(1<<USBS0)|(3<<UCSZ00);
+ //UCSR0C=0x06;
+ iIn=iOut=0; //Initialize buffer pointers
+}
+
+void tx(int cData)
+{
+   while (!(UCSR0A&(1<<UDRE0)));
+    // pu t da ta i n t o b u f f e r
+    UDR0=cData;
+}
+
+int rx()
+{
+   while (!(UCSR0A&(1<<RXC0)));
+    //get and return data
+   cData= UDR0;
+
+   return cData;
+}
+
+int recive_tx()
+{
+   while (!(UCSR0A&(1<<RXC0)));
+    //get and return data
+   cData= UDR0;
+
+   while (!(UCSR0A&(1<<UDRE0)));
+    // pu t da ta i n t o b u f f e r
+    UDR0=cData;
+ 
+   return cData;
+}
+
+
